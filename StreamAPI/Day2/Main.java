60808fbcedb2c903efae1ab79720c6a30715dfdc
StefanParmezan <syromatnikov26@gmail.com>
2025-07-07 19:39:07 +0500

    Learned: - [ ] .filter() - [ ] .map() - [ ] .distinct() - [ ] .sorted()

--- StreamAPI/Day2/Main.java
+++ StreamAPI/Day2/Main.java
@@ -1,7 +1,21 @@
 package StreamAPI.Day2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args){
-
+        WorkingWithList workingWithList = new WorkingWithList();
+        List<String> list = new ArrayList<>();
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Africa");
+        list.add("Cherry");
+        list.add("Apple");
+        System.out.println(workingWithList.filterByValue(list, "A"));
+        System.out.println(workingWithList.toUpperList(list));
+        System.out.println(workingWithList.removeDuplicates(list));
+        System.out.println(workingWithList.toStupidList(list));
+        System.out.println(workingWithList.sortByAlphabet(list));
     }
 }
---
+++ StreamAPI/Day2/WorkingWithList.java
@@ -0,0 +1,30 @@
+package StreamAPI.Day2;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+class WorkingWithList {
+    public List<String> filterByValue(List<String> input, String value){
+        return input.stream().filter(s -> s.startsWith(value)).toList();
+    }
+
+    public List<String> toUpperList(List<String> input){
+        return input.stream()
+                .map(s -> s.toUpperCase())
+                .toList();
+    }
+
+    public List<String> removeDuplicates(List<String> input){
+        return input.stream().distinct().toList();
+    }
+
+    public List<String> toStupidList(List<String> input){
+        return IntStream.range(0, input.size())
+                .mapToObj(i -> i % 2 == 0 ? input.get(i).toUpperCase() : input.get(i).toLowerCase())
+                .toList();
+    }
+
+    public List<String> sortByAlphabet(List<String> input){
+        return input.stream().sorted().toList();
+    }
+ }
